7d0a0ec3597e8a039f0fd1c09d8389f2d44524c9
David Vannucci <davidv@keytelematics.com>
2025-05-28 15:03:11 +0200

    wip, adding to support more user model details

--- Stats2fa/api/models/User.cs
+++ Stats2fa/api/models/User.cs
@@ -73,6 +73,10 @@ public class User {
         [JsonPropertyName("message")]
         public string? Message { get; set; }
     }
+    
+    // TODO support these additional fields that come from a different response but can be combined
+    // {"id":"494c7592-eb05-4b17-a299-5ffaab5924d3","owner":{"id":"d4d4627b-f436-4d97-a25d-057d56d23605","name":"Example Inc","type":"client"},"name":"Johan Havenga (C)","emailAddress":"johanh@keytelematics.com","mobile":"+","timeZoneId":"Africa/Johannesburg","language":"en-us","state":"active","defaultClient":{"id":"d4d4627b-f436-4d97-a25d-057d56d23605","name":"Example Inc"},"roles":[{"id":"00000000-0000-0000-0000-000000000000","name":"Administrator"}],"notifySettings":{"smsTime":{"to":"23:59:59","from":"00:00:00"},"actions":{"low":"none","medium":"none","high":"none"}},"costCentre":{"id":"4e66094e-9220-406c-87b4-e7e4408da089","name":"Gold Coast"},"oidcTags":{},"apiKeys":{},"otp":[],"entity":{"modifiedDate":"2025/05/28 12:08:58","creationDate":"2024/08/30 09:24:43"}}
+    
 }
 
 internal class UserComparer : IEqualityComparer<User> {
